1f701dd98c7d8d603b38329ca898c0d5c2b0b0bd
Bryce Richmond <bryce.richmond@gmail.com>
2025-02-11 11:30:15 -0800

    Added routing

--- workout-tracker/src/App.jsx
+++ workout-tracker/src/App.jsx
@@ -1,17 +1,23 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
-// import LoginPage from "./components/LoginPage"
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from "./components/LoginPage/LoginPage"
 import Homepage from "./components/Homepage/Homepage"
+import Plan from "./components/Plan/Plan.jsx"
+import ExercisePlan from './components/ExercisePage/ExercisePage.jsx'
 
 function App() {
 
   return (
-    <div>
-      <Navbar />
-      <Homepage/>
-      {/* <LoginPage/> */}
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/plan' element={<Plan/>}/>
+        <Route path='/exercise' element={<ExercisePlan/>}/>
+      </Routes>
+    </>
+  );
 }
 
 export default App
