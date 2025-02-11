6962d2ca74128983cc66aebae815e3f96dcb4d52
Wylie Mickelson <wyliecoyote910@gmail.com>
2025-02-10 21:23:38 -0800

    first half of homepage completed

--- workout-tracker/src/App.jsx
+++ workout-tracker/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
+import Homepage from "./components/Homepage/Homepage"
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Homepage />
     </div>
   )
 }
