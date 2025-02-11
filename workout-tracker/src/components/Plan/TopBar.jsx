0f4fc1d0b5135f22e62505df56f42eb226c8eb9c
Tiger Caldwell <caldwet6@wwu.edu>
2025-02-11 11:10:07 -0800

    Breaking Plan page down into more individual components

---
+++ workout-tracker/src/components/Plan/TopBar.jsx
@@ -0,0 +1,22 @@
+function TopBar() {
+    return <div className="flex flex-row justify-between">
+        {/* Left buttons */}
+        <div className="flex flex-row gap-2">
+            {/* Add Plan button */}
+            <button className="rounded-lg bg-black text-white p-1 hover:cursor-pointer">Add Plan</button>
+
+            {/* Month Selector */}
+            <button className="rounded-lg bg-white border border-[#B9B9B9] text-[#898989] p-1 hover:cursor-pointer">February 2025</button>
+
+            {/* Filter button */}
+            <button className="rounded-lg bg-white border border-[#B9B9B9] text-[#898989] p-1 hover:cursor-pointer">Filter</button>
+        </div>
+
+        {/* Search bar */}
+        <div className="bg-white border rounded-lg border-solid border-[#B9B9B9] p-1">
+            <input placeholder="Search Task" className="text-xs h-full outline-none"></input>
+        </div>
+    </div>
+}
+
+export default TopBar;
