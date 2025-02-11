5a521c7b32df8c21bc9bce90a7400aa0e1a2430a
Sara Wooten <saramwoot17@gmail.com>
2025-02-10 18:36:17 -0800

    Created component for login page and added converted html code as react code in the function.

--- workout-tracker/src/App.jsx
+++ workout-tracker/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
+import LoginPage from "./components/Navbar/LoginPage"
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      {/* <LoginPage/> */}
     </div>
   )
 }
