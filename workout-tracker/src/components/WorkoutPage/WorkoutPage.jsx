3116e5af86bd3004c09eef9f518bc4a0c2faafaa
ThorBeck20 <thorbeck15@gmail.com>
2025-02-11 21:43:08 -0800

    custom bad popup way

--- workout-tracker/src/components/WorkoutPage/WorkoutPage.jsx
+++ workout-tracker/src/components/WorkoutPage/WorkoutPage.jsx
@@ -1,14 +1,10 @@
 import "./WorkoutPage.css";
-import { useRef, useState} from "react";
+import Popup from "./Popup";
+import { useRef, useState } from "react";
 
-const AddWorkoutDialog = () => {
+const WorkoutPage = () => {
   const [exercise, setExercise] = useState([{ name: "", sets: "", reps: "", weight: ""}]);
-  const dialogRef = useRef<HTMLDialogElement>(null);
-  
-
-  // const addWorkout = () => {
-  //   setWorkouts([...workouts, {exercise: "", sets: "", reps: "", weight: ""}]);
-  // }  
+  const [showPopup, setShowPopup] = useState(false);
 
   function handleChange(e) {
     const name = e.target.name;
@@ -23,78 +19,104 @@ const AddWorkoutDialog = () => {
         reps: reps
     }]);
   }
-  
-  const openDialog = () => {
-    dialogRef.current?.showModal();
-  }
-
-  const closeDialog = () => {
-    dialogRef.current?.close();
-  }
 
   return (
-    <div className="workout-page container">
-        {/* Workout header */}
-        <div className="pop-up-info">
-            <h1>Add Workout</h1>
-            <p>Please input information for your exercise.</p>
-        </div>
+    <div className="workout-page flex justify-center">
+        <button className="btn1 py-32" onClick={() => setShowPopup(true)}>Open Popup</button>
+        <Popup trigger={showPopup} setTrigger={setShowPopup}>
+            <div className="container flex-none basis-1/3">
+            {/* Workout header */}
+            <div className="pop-up-info">
+                <div className="flex justify-between">
+                    <h1>Add Workout</h1>
+                    <button className="close-btn flex px-4" onClick={() => setShowPopup(false)}>close</button>
+                </div>
+                <p>Please input information for your exercise.</p>
+            </div>
 
-        {/* Dropdown for Workout Type */}
-        <div className="workout-type container">
-            <h1>Workout Type</h1>
-            <select className="">
-                <option value="Anaerobic">Anaerobic</option>
-                <option value="aerobic">Aerobic</option>
-            </select>
-        </div>
+            {/* Dropdown for Workout Type */}
+            <div className="workout-type">
+                <div>
+                    <h1>Workout Type</h1>
+                </div>
+                <select className="flex justify-center">
+                    <option value="Anaerobic">Anaerobic</option>
+                    <option value="aerobic">Aerobic</option>
+                </select>
+            </div>
 
-        {/* Input section */}
-        <div className="input-selection container">
-            <form>
-                <ul>
-                    <li>
+            {/* Separator */}
+            <div className="separator">
+                <div className="border"></div>
+            </div>
+
+            {/* Input section */}
+            <div className="input-section">
+            {/* <form className="flex flex-wrap justify-center gap-4"> */}
+                <form className="grid grid-cols-2 gap-4">
+                    <div className="">
                         <h2>Exercise Name</h2>
-                        <input value={exercise.name} type="text" onChange={handleChange}></input>
-                    </li>
-                    <li>
+                        <input
+                            value={exercise.name}
+                            placeholder="Enter name"
+                            type="text" 
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="">
                         <h2>Starting Weight</h2>
-                        <input value={exercise.weight} type="text" onChange={handleChange}></input>
-                    </li>
-                    <li>
+                        <input
+                            value={exercise.weight}
+                            placeholder="Enter weight"
+                            type="text"
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="">
                         <h2>Sets</h2>
-                        <input value={exercise.sets} type="text" onChange={handleChange}></input>
-                    </li>
-                    <li>
+                        <input 
+                            value={exercise.sets}
+                            placeholder="Enter sets"
+                            type="text"
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="">
                         <h2>Reps</h2>
-                        <input value={exercise.reps} type="text" onChange={handleChange}></input>
+                        <input
+                            value={exercise.reps}
+                            placeholder="Enter reps"
+                            type="text"
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                </form>
+            </div>
+
+            {/* Add another exercise */}
+            <div className="add-exercise">
+                <button className="btn1">+ Add another exercise</button>
+            </div>
+
+            {/* Buttons */}
+            <div className="px-16">
+                <ul className="flex flex-row justify-end gap-4">
+                    <li className="basis-16">
+                        <button onClick={() => setShowPopup(false)} className="btn1">
+                            <div className="cancel-text">Cancel</div>
+                        </button>
+                    </li>
+                    <li className="basis-16">
+                        <button className="btn2">
+                            <div className="submit-text">Submit</div>
+                        </button>
                     </li>
                 </ul>
-            </form>
+            </div>
         </div>
-
-        {/* Add another exercise */}
-        <div className="container">
-            <button className="btn1">+ Add another exercise</button>
-        </div>
-
-        {/* Buttons */}
-        <div className="container">
-            <ul className="flex flex-row justify-end">
-                <li className="basis-16 px-32">
-                    <button onClick={closeDialog} className="btn1">
-                        <div className="cancel-text">Cancel</div>
-                    </button>
-                </li>
-                <li className="basis-16">
-                    <button className="btn2">
-                        <div className="submit-text">Submit</div>
-                    </button>
-                </li>
-            </ul>
-        </div>
+        </Popup>
     </div>
   );
 };
 
-export default AddWorkoutDialog;
+export default WorkoutPage;
