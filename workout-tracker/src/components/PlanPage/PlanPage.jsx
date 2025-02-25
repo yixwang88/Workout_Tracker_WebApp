9b112d751aa1693752ebd3e7c5419d7f7fe2f77f
Tiger Caldwell <caldwet6@wwu.edu>
2025-02-24 19:39:24 -0800

    Adding dates and times to the Planner

--- workout-tracker/src/components/PlanPage/PlanPage.jsx
+++ workout-tracker/src/components/PlanPage/PlanPage.jsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import Planner from "./Planner";
 import TopBar from "./TopBar";
 
-const Plan = function () {
+const PlanPage = function () {
+    const [dateFilter, setDateFilter] = useState(new Date())
+
     return (
         <>
             <Navbar />
             <div className="flex gap-4 box-border h-screen flex-col grow bg-[#FAFAFA] p-10">
-                <TopBar />
-                <Planner />
+                <TopBar date={dateFilter} onSetDate={setDateFilter} />
+                <Planner dateFilter={dateFilter} />
             </div>
         </>
     )
 }
 
-export default Plan;
+export default PlanPage;
