f486dd2e753a77bd6cdab32086f62b44c084cd4d
Tiger Caldwell <caldwet6@wwu.edu>
2025-03-10 12:11:09 -0700

    Merged both projects into a single package.json for easier deployment

--- workout-tracker/src/components/WorkoutPage/AddWorkoutModal/AddWorkoutModal.jsx
+++ workout-tracker/src/components/WorkoutPage/AddWorkoutModal/AddWorkoutModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./addWorkoutModal.css"
+import "./AddWorkoutModal.css"
 import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
