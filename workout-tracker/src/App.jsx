bde2bbc832e0d5c2888189a596b3d9f934cc202a
Bryce Richmond <bryce.richmond@gmail.com>
2025-03-12 01:05:47 -0700

    Forgot app

--- workout-tracker/src/App.jsx
+++ workout-tracker/src/App.jsx
@@ -8,6 +8,7 @@ import ExercisePlan from './components/ExercisePage/ExercisePage.jsx'
 import WorkoutPage from "./components/WorkoutPage/WorkoutPage.jsx"
 import CurrentWorkout from "./components/CurrentWorkout/CurrentWorkout.jsx"
 import PathNotFound from "./components/PathNotFound/pathNotFound.jsx"
+import GetStarted from "./components/ExercisePage/GetStarted.jsx"
 import { Toaster } from "react-hot-toast"
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/plan' element={<PlanPage/>}/>
         <Route path='/exercise' element={<ExercisePlan/>}/>
+        <Route path="/exercise/get-started" element={<GetStarted/>}/>
         <Route path='/add-workout' element={<WorkoutPage/>}/>
         <Route path="/current-workout" element={<CurrentWorkout/>} />
         <Route path="/*" element={<PathNotFound/>}></Route>
